2422e5c3e08aba772263b9b97ced76b3c864bcc1
G00405094 <G00405094@atu.ie>
2023-11-08 14:14:32 +0000

    Created ProductService class

---
+++ src/main/java/ie/atu/productservice/ProductService.java
@@ -0,0 +1,13 @@
+package ie.atu.productservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductService {
+    private String addProduct;
+    private String getProductId;
+}
